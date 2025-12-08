0e1f3ec28c4802d232e0fc0cf8646ed5c574b7e9
François Ulrich <fulrich3@gmail.com>
2025-12-08 10:14:31 +0100

    added board creation form in HomeView modal

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -5,19 +5,33 @@ import { useBoardStore } from "@/stores/board"
 import type { Board } from "@/types/board"
 import BoardItem from "@/views/Home/BoardItem.vue"
 import { ref } from "vue"
+import TextInput from "@/components/form/TextInput.vue"
+import BaseButton from "@/components/BaseButton.vue"
 
 const boardStore = useBoardStore()
 
 const boards = ref<Board[]>(boardStore.items)
 
+const cardEditForm = ref<{ name: string }>({
+    name: "",
+})
+
 const showBoardCreationModal = ref<boolean>(false)
 
-const handleModalOpen = () => {
+const handleBoardCreationModalOpen = () => {
     showBoardCreationModal.value = true
 }
 
-const handleModalClose = () => {
+const handleBoardCreationModalClose = () => {
     showBoardCreationModal.value = false
+}
+
+const handleBoardCreationFormSubmit = () => {
+    if (cardEditForm.value.name.length === 0) return
+    const newBoard = { name: cardEditForm.value.name }
+    boardStore.create(newBoard)
+    cardEditForm.value.name = ""
+    handleBoardCreationModalClose()
 }
 </script>
 
@@ -34,7 +48,7 @@ const handleModalClose = () => {
             <li>
                 <BoardItemBase
                     class="bg-gray-200 flex justify-center items-center text-gray-600 font-semibold text-sm"
-                    @click="handleModalOpen"
+                    @click="handleBoardCreationModalOpen"
                 >
                     <p>Create new board</p>
                 </BoardItemBase>
@@ -42,10 +56,23 @@ const handleModalClose = () => {
         </ul>
 
         <Teleport to="body" v-if="showBoardCreationModal">
-            <ModalDialog @close="handleModalClose">
+            <ModalDialog @close="handleBoardCreationModalClose">
                 <template #header>
                     <p class="font-medium">Create new board</p>
                 </template>
+
+                <form @submit.prevent="handleBoardCreationFormSubmit" class="flex flex-col gap-2">
+                    <TextInput
+                        id="name"
+                        label="Name"
+                        v-model="cardEditForm.name"
+                        class="w-full mb-2"
+                    />
+
+                    <div>
+                        <BaseButton type="submit">Create board</BaseButton>
+                    </div>
+                </form>
             </ModalDialog>
         </Teleport>
     </div>
