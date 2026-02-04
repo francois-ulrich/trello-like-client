ea475737e6a2f30f3b34a5dcee7959101a9d2d59
François Ulrich <fulrich3@gmail.com>
2026-02-04 11:32:57 +0100

    added createLocalStoragePlugin factory for local storage pinia store plugin with options argument

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import "./style.css"
 import App from "./App.vue"
-import localStoragePlugin from "@/plugins/localStoragePlugin"
+import { createLocalStoragePlugin } from "@/plugins/localStoragePlugin"
 import router from "@/router"
 
 const app = createApp(App)
 
 const pinia = createPinia()
-pinia.use(localStoragePlugin)
+
+pinia.use(createLocalStoragePlugin({ ignoredStores: ["auth"] }))
 app.use(pinia)
 
 app.use(router)
--- src/plugins/localStoragePlugin.ts
+++ src/plugins/localStoragePlugin.ts
@@ -1,19 +1,28 @@
 import type { PiniaPluginContext } from "pinia"
 
-const localStoragePlugin = (context: PiniaPluginContext) => {
-    const { store } = context
-
-    const keyPrefix = `trelloLike-${store.$id}`
-
-    const storedState = localStorage.getItem(keyPrefix)
-
-    if (storedState) {
-        store.$patch(JSON.parse(storedState))
-    }
-
-    store.$subscribe((_, state) => {
-        localStorage.setItem(keyPrefix, JSON.stringify(state))
-    })
+interface LocalStoragePluginOptions {
+    ignoredStores: string[]
 }
 
-export default localStoragePlugin
+export function createLocalStoragePlugin(options: LocalStoragePluginOptions | undefined) {
+    return (context: PiniaPluginContext) => {
+        const { store } = context
+
+        const ignoredStoreIds: string[] = options !== undefined ? options.ignoredStores : []
+
+        const keyPrefix = `trelloLike-${store.$id}`
+
+        const storedState = localStorage.getItem(keyPrefix)
+
+        if (storedState) {
+            store.$patch(JSON.parse(storedState))
+        }
+
+        store.$subscribe((_, state) => {
+            if (ignoredStoreIds.includes(store.$id)) return
+            localStorage.setItem(keyPrefix, JSON.stringify(state))
+        })
+    }
+}
+
+export default createLocalStoragePlugin
